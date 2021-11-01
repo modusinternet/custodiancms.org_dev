932573d28311d8c98f956a0f714a9338ad4a20a1
Vince <vince@modusinternet.com>
2021-11-01 09:39:08 -0700

    asdf

--- ccmsusr/index.php
+++ ccmsusr/index.php
@@ -51,7 +51,7 @@ if($_SESSION["FAIL"] >= 5) {
 }
 
 
-echo "ccms_tpl = [" . $CLEAN["ccms_tpl"] . "]\n\n";
+echo "ccms_tpl = [" . $CLEAN["ccms_tpl"] . "]<br>\n\n";
 //exit;
 
 
@@ -59,7 +59,7 @@ if(!isset($_SESSION["USER_ID"]) || isset($_POST["ccms_login"]) || isset($_POST["
 	$CLEAN["ccms_tpl"] = "/user/login.html";
 }
 
-echo "ccms_tpl = [" . $CLEAN["ccms_tpl"] . "]\n\n";
+echo "ccms_tpl = [" . $CLEAN["ccms_tpl"] . "]<br>\n\n";
 exit;
 
 CCMS_Main();
