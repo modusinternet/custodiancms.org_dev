bd9ed63812823cfa2662d6a371de3a8841cbfa7d
Vince <vince@modusinternet.com>
2021-10-31 22:13:50 -0700

    asdf

--- ccmsusr/index.php
+++ ccmsusr/index.php
@@ -2,6 +2,11 @@
 // benchmark start
 //$time_start = microtime(true);
 
+
+echo "/ccmsusr/index.html";
+exit;
+
+
 // Use the ini_set function to set value of the include_path option on your server if necessary.
 // e.g.: ini_set('include_path', 'ccmslib:ccmspre:ccmstpl:ccmsusr' . ini_get('include_path'));
 //ini_set('include_path', $CFG["LIBDIR"] . ':' . $CFG["PREDIR"] . ':' . $CFG["TPLDIR"] . ':' . $CFG["USRDIR"] . ':' . ini_get('include_path'));
