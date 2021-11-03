13ada63fec7e96b03b26de80146aad440cad2437
Vince <vince@modusinternet.com>
2021-11-03 09:59:40 -0700

    asdf

--- ccmsusr/index.php
+++ ccmsusr/index.php
@@ -55,10 +55,12 @@ if(!isset($_SESSION["USER_ID"]) || isset($_POST["ccms_login"]) || isset($_POST["
 }
 
 //echo "ccms_tpl = [" . $CLEAN["ccms_tpl"] . "]<br>\n\n";
-header("ccms_tpl: " . $CLEAN["ccms_tpl"]);
+header("ccms_tpl1: " . $CLEAN["ccms_tpl"]);
 //exit;
 
 CCMS_Main();
+
+header("ccms_tpl2: " . $CLEAN["ccms_tpl"]);
 
 // benchmark end
 //echo 'Total execution time in seconds: ' . (microtime(true) - $time_start);
