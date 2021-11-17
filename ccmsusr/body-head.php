f40122c76b288d87b496579a3bfcf375c394b61e
Vince <vince@modusinternet.com>
2021-11-17 13:08:33 -0800

    asdf

--- ccmsusr/body-head.php
+++ ccmsusr/body-head.php
@@ -6,6 +6,16 @@
 		</a>
 	</div>
 	<div id="menu-ctn" style="float:right">
+
+		<div class="dropdown">
+			<button onclick="myFunction()" class="dropbtn">Dropdown</button>
+			<div id="myDropdown" class="dropdown-content">
+				<a href="#home">Home</a>
+				<a href="#about">About</a>
+				<a href="#contact">Contact</a>
+			</div>
+		</div>
+
 		<li class="menu-bars"></li>
 	</div>
 </header>
