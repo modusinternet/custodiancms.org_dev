6b742c9043406bc72442bade666449e1091fe7a0
Vince <vince@modusinternet.com>
2021-11-18 10:43:05 -0800

    asdf

--- ccmsusr/body-head.php
+++ ccmsusr/body-head.php
@@ -11,16 +11,16 @@
 	</div>
 
 	<div class="dropdown" style="float:right">
-		<button class="dropbtn" id="user_dropdown_btn">
 
 
 
 
-		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" style="fill:rgb(241, 158, 0);filter:drop-shadow(2px 2px 4px rgba(0,0,0,.2));width:30px"><path d="M15 11h7v2h-7zm1 4h6v2h-6zm-2-8h8v2h-8zM4 19h10v-1c0-2.757-2.243-5-5-5H7c-2.757 0-5 2.243-5 5v1h2zm4-7c1.995 0 3.5-1.505 3.5-3.5S9.995 5 8 5 4.5 6.505 4.5 8.5 6.005 12 8 12z"></path></svg>
+
+		<svg class="dropbtn" id="user_dropdown_btn" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M15 11h7v2h-7zm1 4h6v2h-6zm-2-8h8v2h-8zM4 19h10v-1c0-2.757-2.243-5-5-5H7c-2.757 0-5 2.243-5 5v1h2zm4-7c1.995 0 3.5-1.505 3.5-3.5S9.995 5 8 5 4.5 6.505 4.5 8.5 6.005 12 8 12z"></path></svg>
 
 
 
-	</button>
+
 		<div id="user_dropdown" class="dropdown-content">
 			<a href="#home">Home</a>
 			<a href="#about">About</a>
