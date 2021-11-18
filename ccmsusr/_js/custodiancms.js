be604cfeb8f13199abc04a7af87117ccf249ed4a
Vince <vince@modusinternet.com>
2021-11-17 17:42:39 -0800

    asdf

--- ccmsusr/_js/custodiancms.js
+++ ccmsusr/_js/custodiancms.js
@@ -29,21 +29,23 @@ navActiveItem.forEach(function(nl){$("#"+nl+">a").addClass("active");});
 toggle between hiding and showing the dropdown content */
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
+
+	var dropdowns = document.getElementsByClassName("dropdown-content");
+	var i;
+	for (i = 0; i < dropdowns.length; i++) {
+		var openDropdown = dropdowns[i];
+		if (openDropdown.classList.contains('show')) {
+			openDropdown.classList.remove('show');
+		}
+	}
+	
 }
 
 // Close the dropdown if the user clicks outside of it
 document.addEventListener('DOMContentLoaded', function () {
-	document.getElementById('user_dropdown')
-		.addEventListener('click', myFunction);
+	document.getElementById('user_dropdown').addEventListener('click', myFunction);
 
-		var dropdowns = document.getElementsByClassName("dropdown-content");
-		var i;
-		for (i = 0; i < dropdowns.length; i++) {
-			var openDropdown = dropdowns[i];
-			if (openDropdown.classList.contains('show')) {
-				openDropdown.classList.remove('show');
-			}
-		}
+
 
 });
 
