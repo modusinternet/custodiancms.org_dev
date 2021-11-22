ea541633e91e0ea8fff6a4384a92b338091b0bfa
Vince <vince@modusinternet.com>
2021-11-22 10:42:05 -0800

    asdf

--- ccmsusr/body-head.php
+++ ccmsusr/body-head.php
@@ -21,7 +21,7 @@
 				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="#6563ff" d="M21.66,10.25l-9-8a1,1,0,0,0-1.32,0l-9,8a1,1,0,0,0-.27,1.11A1,1,0,0,0,3,12H4v9a1,1,0,0,0,1,1H19a1,1,0,0,0,1-1V12h1a1,1,0,0,0,.93-.64A1,1,0,0,0,21.66,10.25ZM13,20H11V17a1,1,0,0,1,2,0Zm5,0H15V17a3,3,0,0,0-6,0v3H6V12H18ZM5.63,10,12,4.34,18.37,10Z"/></svg>
 				<!-- a href="https://iconscout.com/">Unicons by Iconscout</a -->
 				Back to Home</a>
-			<a href="#contact" style="border-top:1px solid var(--cl0)">
+			<a href="/{CCMS_LIB:_default.php;FUNC:ccms_lng}/user/?ccms_logout=1" style="border-top:1px solid var(--cl0)">
 				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="#6563ff" d="M4,12a1,1,0,0,0,1,1h7.59l-2.3,2.29a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0l4-4a1,1,0,0,0,.21-.33,1,1,0,0,0,0-.76,1,1,0,0,0-.21-.33l-4-4a1,1,0,1,0-1.42,1.42L12.59,11H5A1,1,0,0,0,4,12ZM17,2H7A3,3,0,0,0,4,5V8A1,1,0,0,0,6,8V5A1,1,0,0,1,7,4H17a1,1,0,0,1,1,1V19a1,1,0,0,1-1,1H7a1,1,0,0,1-1-1V16a1,1,0,0,0-2,0v3a3,3,0,0,0,3,3H17a3,3,0,0,0,3-3V5A3,3,0,0,0,17,2Z"/></svg>
 				<!-- a href="https://iconscout.com/">Unicons by Iconscout</a -->
 				Logout</a>
