f83b2137afe12c03bec56493fae49b2d9e80faeb
Vince <vince@modusinternet.com>
2021-11-22 11:44:28 -0800

    asdf

--- ccmsusr/index.php
+++ ccmsusr/index.php
@@ -46,7 +46,7 @@ if(($_SESSION["FAIL"] ?? null) >= 5) {
 	exit;
 }
 
-header("ccms_logout: ".$_GET["ccms_logout"]);
+header("ccms_logout: ".$_REQUEST["ccms_logout"]);
 
 if(!isset($_SESSION["USER_ID"]) || isset($_POST["ccms_login"]) || isset($_POST["ccms_logout"]) || isset($_POST["ccms_pass_reset_part_1"]) || isset($_POST["ccms_pass_reset_part_2"])) {
 	$CLEAN["ccms_tpl"] = "/login.php";
