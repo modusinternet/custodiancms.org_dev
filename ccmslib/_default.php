a859808c3d028eb95d907982cf73c0c1240ce960
Vince <vince@modusinternet.com>
2021-11-23 10:55:01 -0800

    asdf

--- ccmslib/_default.php
+++ ccmslib/_default.php
@@ -179,7 +179,7 @@ function ccms_user_admin_slider() {
 <input type="checkbox" id="CCMSTab-slide-tab-checkbox" onclick="ccms_tab_switch();">
 <div id="CCMSTab-slide">
 	<label id="CCMSTab-slide-tab" for="CCMSTab-slide-tab-checkbox" title="User Admin Slider">
-		<i class="fa fa-cogs fa-spin"></i>
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="#fff" d="M21,12a1,1,0,0,0-1,1v6a1,1,0,0,1-1,1H5a1,1,0,0,1-1-1V5A1,1,0,0,1,5,4h6a1,1,0,0,0,0-2H5A3,3,0,0,0,2,5V19a3,3,0,0,0,3,3H19a3,3,0,0,0,3-3V13A1,1,0,0,0,21,12ZM6,12.76V17a1,1,0,0,0,1,1h4.24a1,1,0,0,0,.71-.29l6.92-6.93h0L21.71,8a1,1,0,0,0,0-1.42L17.47,2.29a1,1,0,0,0-1.42,0L13.23,5.12h0L6.29,12.05A1,1,0,0,0,6,12.76ZM16.76,4.41l2.83,2.83L18.17,8.66,15.34,5.83ZM8,13.17l5.93-5.93,2.83,2.83L10.83,16H8Z"/></svg>
 	</label>
 	<div id="CCMSTab-slide-outer">
 		<div id="CCMSTab-slide-inner">
