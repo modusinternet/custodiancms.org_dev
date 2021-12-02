5e7968567ce980d5450882ab4744ba9cc64e0c2f
modusinternet <vince@modusinternet.com>
2021-12-02 10:26:48 -0800

    asdf

--- ccmstpl/examples/_js/serviceworker.php
+++ ccmstpl/examples/_js/serviceworker.php
@@ -72,9 +72,9 @@ self.addEventListener('fetch',(event) => {
 	console.log('SW fetch event.', event.request.method, event.request.url);
 	/*
 		This example demonstrates how to avoid doing a serviceWorker cache of templates if they are coming from WordPress folders, Google RECAPTCHA or the CustodianCMS 'user' folder/admin.
-		if(!/\/wp\-(.*)|\/recaptcha\/|(\/(([a-z]{2})(-[a-z]{2})?)\/user\/)/i.test(event.request.url)) {
+		if(!/\/wp\-(.*)|\/recaptcha\/|(\/(([a-z]{2,3})(-[a-z0-9]{2,3})?)\/user\/)/i.test(event.request.url)) {
 	*/
-	if(!/\/recaptcha\/|(\/(([a-z]{2})(-[a-z]{2})?)\/user\/)/i.test(event.request.url)){
+	if(!/\/recaptcha\/|(\/(([a-z]{2,3})(-[a-z0-9]{2,3})?)\/user\/)/i.test(event.request.url)){
 		event.respondWith(
 			caches.open(cacheName).then(cache => {
 				return cache.match(event.request).then(response => {
@@ -97,7 +97,7 @@ self.addEventListener('fetch',(event) => {
 					});
 					return response || fetchPromise;
 				}).catch(function() {
-					/* The template being called was not found in cache and there is not internet connection at the moment so display the offline page instead.  The code below makes sure we're dispalying the appropriate offline template for the language that's currently selected by the client. */
+					/* The template being called was not found in cache and there is no internet connection at the moment so display the offline page instead.  The code below makes sure we're dispalying the appropriate offline template for the language that's currently selected by the client. */
 					const regex = /\/(([a-z]{2,3})(-[a-z0-9]{2,3})?)\//i;
 					const lng = event.request.url.match(regex);
 					const searchForThis = '/' + lng[1] + '/examples/offline.html';
