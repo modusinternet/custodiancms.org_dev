4c8445a5c63f411bd4a001c94580aa80f54508cc
modusinternet <vince@modusinternet.com>
2021-12-15 00:43:13 -0800

    asdf

--- ccmsusr/dashboard/index.php
+++ ccmsusr/dashboard/index.php
@@ -45,7 +45,7 @@ if($_SERVER["SCRIPT_NAME"] != "/ccmsusr/index.php") {
 		<main style="padding:20px 20px 20px 0">
 			<h1 style="padding-bottom:20px;margin:40px 0 20px;border-bottom:1px solid #eee">Dashboard</h1>
 
-			<p>This section is still under development.  If you come accross any unresolved issues please let us know at: <a href="mailto:info@custodiancms.org?subject=unresolved+issue+report">info@custodiancms.org</a></p>
+			<p>This section is still under development, but if you come across any unresolved issues please let us know at: <a href="mailto:info@custodiancms.org?subject=unresolved+issue+report">info@custodiancms.org</a></p>
 
 			<div class="modal">
 				<div>System Info</div>
