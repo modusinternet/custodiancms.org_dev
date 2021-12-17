bcabbf610b6476e747a91110e34bc687bc0b40c1
modusinternet <vince@modusinternet.com>
2021-12-17 03:08:07 -0800

    asdf

--- ccmsusr/dashboard/index.php
+++ ccmsusr/dashboard/index.php
@@ -201,7 +201,7 @@ if($_SERVER["SCRIPT_NAME"] != "/ccmsusr/index.php") {
 				}
 			}
 
-			document.getElementById("ccms_news_reload").addEventListener("click", function() {
+			document.getElementById("ccms_news_reload").addEventListener("click", () => {
 				localStorage.removeItem("spdemowebFonts");
 				injectFontsStylesheet();
 			});
@@ -217,18 +217,75 @@ if($_SERVER["SCRIPT_NAME"] != "/ccmsusr/index.php") {
 				}
 			}
 
-			if(localStorageSupported() && localStorage.spdemowebFonts) {
+			if(localStorageSupported() && localStorage.spdemowebFontsExp && localStorage.spdemowebFonts) {
+				const value = getWithExpiry("spdemowebFontsExp","spdemowebFonts")
 				injectRawStyle(localStorage.getItem('spdemowebFonts'));
+
+
+
 			} else {
+				setWithExpiry("spdemowebFontsExp", inputSet.value, 10000)
 				injectFontsStylesheet();
 			}
 
 
 
 
-var d1 = new Date();
-localStorage.setItem("key", d1.getTime());
-var d2 = new Date(parseInt(localStorage.getItem[key]));
+
+
+
+
+
+
+
+
+/*
+btnSet.addEventListener("click", () => {
+	setWithExpiry("myKey", inputSet.value, 5000)
+})
+
+btnGet.addEventListener("click", () => {
+	const value = getWithExpiry("myKey")
+	valueDisplay.innerHTML = value
+})
+*/
+
+function setWithExpiry(key, value, ttl) {
+	const now = new Date()
+
+	// `item` is an object which contains the original value
+	// as well as the time when it's supposed to expire
+	const item = {
+		value: value,
+		expiry: now.getTime() + ttl,
+	}
+	localStorage.setItem(key, JSON.stringify(item))
+}
+
+function getWithExpiry(keyExp, key) {
+	const keyExpStr = localStorage.getItem(keyExp)
+	const keyStr = localStorage.getItem(key)
+
+	// if the item doesn't exist, return null
+	if (!keyExpStr) {
+		return null
+	}
+
+	const item = JSON.parse(keyExpStr)
+	const now = new Date()
+
+	// compare the expiry time of the item with the current time
+	if (now.getTime() > item.expiry) {
+		// If the item is expired, delete the item from storage
+		// and return null
+		localStorage.removeItem(keyExp)
+		localStorage.removeItem(key)
+		return null
+	}
+	return item.value
+}
+
+
 
 
 
