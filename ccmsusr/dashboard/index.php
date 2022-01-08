21dc7838c9e34e3f6ef3cf9daf3acdd42f388c22
modusinternet <vince@modusinternet.com>
2022-01-07 16:21:57 -0800

    asdf

--- ccmsusr/dashboard/index.php
+++ ccmsusr/dashboard/index.php
@@ -216,69 +216,35 @@ if($_SERVER["SCRIPT_NAME"] != "/ccmsusr/index.php") {
 
 
 
-
-
-var tabledata = <?php
-	$qry = $CFG["DBH"]->prepare("SELECT * FROM `ccms_log`;");
-	if($qry->execute()) {
-		/*
-		while($row = $qry->fetch()) {
-			echo "<p>" . $row["date"] . ", " . $row["ip"] . ", " . $row["url"] . ", " . $row["log"] . "</p>\n";
-		}
-		*/
-		$data = [
-			[id=>1, name=>"Oli Bob", progress=>12, location=>"United Kingdom", gender=>"male", rating=>1, col=>"red", dob=>"14/04/1984", car=>1, lucky_no=>5, lorem=>"Lorem ipsum dolor sit amet, elit consectetur adipisicing"],
-			[id=>2, name=>"Mary May", progress=>1, location=>"Germany", gender=>"female", rating=>2, col=>"blue", dob=>"14/05/1982", car=>true, lucky_no=>10, lorem=>"Lorem ipsum dolor sit amet, elit consectetur adipisicing"],
-			[id=>3, name=>"Christine Lobowski", progress=>42, location=>"France", gender=>"female", rating=>0, col=>"green", dob=>"22/05/1982", car=>"true", lucky_no=>12, lorem=>"Lorem ipsum dolor sit amet, elit consectetur adipisicing"],
-			[id=>4, name=>"Brendon Philips", progress=>100, location=>"USA", gender=>"male", rating=>1, col=>"orange", dob=>"01/08/1980", car=>false, lucky_no=>18, lorem=>"Lorem ipsum dolor sit amet, elit consectetur adipisicing"],
-			[id=>5, name=>"Margret Marmajuke", progress=>16, location=>"Canada", gender=>"female", rating=>5, col=>"yellow", dob=>"31/01/1999", car=>false, lucky_no=>33, lorem=>"Lorem ipsum dolor sit amet, elit consectetur adipisicing"],
-			[id=>6, name=>"Frank Harbours", progress=>38, location=>"Russia", gender=>"male", rating=>4, col=>"red", dob=>"12/05/1966", car=>1, lucky_no=>2, lorem=>"Lorem ipsum dolor sit amet, elit consectetur adipisicing"],
-			[id=>7, name=>"Jamie Newhart", progress=>23, location=>"India", gender=>"male", rating=>3, col=>"green", dob=>"14/05/1985", car=>true, lucky_no=>63, lorem=>"Lorem ipsum dolor sit amet, elit consectetur adipisicing"],
-			[id=>8, name=>"Gemma Jane", progress=>60, location=>"China", gender=>"female", rating=>0, col=>"red", dob=>"22/05/1982", car=>"true", lucky_no=>72, lorem=>"Lorem ipsum dolor sit amet, elit consectetur adipisicing"],
-			[id=>9, name=>"Emily Sykes", progress=>42, location=>"South Korea", gender=>"female", rating=>1, col=>"maroon", dob=>"11/11/1970", car=>false, lucky_no=>44, lorem=>"Lorem ipsum dolor sit amet, elit consectetur adipisicing"],
-			[id=>10, name=>"James Newman", progress=>73, location=>"Japan", gender=>"male", rating=>5, col=>"red", dob=>"22/03/1998", car=>false, lucky_no=>9, lorem=>"Lorem ipsum dolor sit amet, elit consectetur adipisicing"],
-		];
-
-		echo(json_encode($data));
-	}
-?>;
-
-
-
-
-
-
-
-
-
-
 //Create Date Editor
-var dateEditor = function(cell, onRendered, success, cancel){jQuery(function($){
-	$('.table').footable({
-		"columns":[
-			{"name":"id", "title": "ID", "breakpoints": "xs" },
-			{"name":"firstName", "title": "First Name" },
-			{"name":"lastName", "title": "Last Name" },
-			{"name":"jobTitle", "title": "Job Title", "breakpoints": "xs" },
-			{"name":"started", "title": "Started On", "breakpoints": "xs sm" },
-			{"name":"dob", "title": "DOB", "breakpoints": "xs sm md" }
-		],
-		"rows": [
-			{
-				"options": {
-					"expanded": true
+var dateEditor = function(cell, onRendered, success, cancel) {
+	jQuery(function($) {
+		$('.table').footable({
+			"columns":[
+				{"name":"id", "title": "ID", "breakpoints": "xs" },
+				{"name":"firstName", "title": "First Name" },
+				{"name":"lastName", "title": "Last Name" },
+				{"name":"jobTitle", "title": "Job Title", "breakpoints": "xs" },
+				{"name":"started", "title": "Started On", "breakpoints": "xs sm" },
+				{"name":"dob", "title": "DOB", "breakpoints": "xs sm md" }
+			],
+			"rows": [
+				{
+					"options": {
+						"expanded": true
+					},
+					"value": { "id": 1, "firstName": "Dennise", "lastName": "Fuhrman", "jobTitle": "High School History Teacher", "started": "November 8th 2011", "dob": "July 25th 1960" }
 				},
-				"value": { "id": 1, "firstName": "Dennise", "lastName": "Fuhrman", "jobTitle": "High School History Teacher", "started": "November 8th 2011", "dob": "July 25th 1960" }
-			},
-			{ "id": 2, "firstName": "Elodia", "lastName": "Weisz", "jobTitle": "Wallpaperer Helper", "started": "October 15th 2010", "dob": "March 30th 1982" },
-			{ "id": 3, "firstName": "Raeann", "lastName": "Haner", "jobTitle": "Internal Medicine Nurse Practitioner", "started": "November 28th 2013", "dob": "February 26th 1966" },
-			{ "id": 4, "firstName": "Junie", "lastName": "Landa", "jobTitle": "Offbearer", "started": "October 31st 2010", "dob": "March 29th 1966" },
-			{ "id": 5, "firstName": "Solomon", "lastName": "Bittinger", "jobTitle": "Roller Skater", "started": "December 29th 2011", "dob": "September 22nd 1964" },
-			{ "id": 6, "firstName": "Bar", "lastName": "Lewis", "jobTitle": "Clown", "started": "November 12th 2012", "dob": "August 4th 1991" },
-			{ "id": 7, "firstName": "Usha", "lastName": "Leak", "jobTitle": "Ships Electronic Warfare Officer", "started": "August 14th 2012", "dob": "November 20th 1979" },
-			{ "id": 8, "firstName": "Lorriane", "lastName": "Cooke", "jobTitle": "Technical Services Librarian", "started": "September 21st 2010", "dob": "April 7th 1969" }
-		]
-	});
+				{ "id": 2, "firstName": "Elodia", "lastName": "Weisz", "jobTitle": "Wallpaperer Helper", "started": "October 15th 2010", "dob": "March 30th 1982" },
+				{ "id": 3, "firstName": "Raeann", "lastName": "Haner", "jobTitle": "Internal Medicine Nurse Practitioner", "started": "November 28th 2013", "dob": "February 26th 1966" },
+				{ "id": 4, "firstName": "Junie", "lastName": "Landa", "jobTitle": "Offbearer", "started": "October 31st 2010", "dob": "March 29th 1966" },
+				{ "id": 5, "firstName": "Solomon", "lastName": "Bittinger", "jobTitle": "Roller Skater", "started": "December 29th 2011", "dob": "September 22nd 1964" },
+				{ "id": 6, "firstName": "Bar", "lastName": "Lewis", "jobTitle": "Clown", "started": "November 12th 2012", "dob": "August 4th 1991" },
+				{ "id": 7, "firstName": "Usha", "lastName": "Leak", "jobTitle": "Ships Electronic Warfare Officer", "started": "August 14th 2012", "dob": "November 20th 1979" },
+				{ "id": 8, "firstName": "Lorriane", "lastName": "Cooke", "jobTitle": "Technical Services Librarian", "started": "September 21st 2010", "dob": "April 7th 1969" }
+			]
+		});
+	}
 });
 
 
