95bdbc860547d6331293a4e0f321f139dd90690b
modusinternet <vince@modusinternet.com>
2022-01-12 21:57:15 -0800

    asdf

--- ccmsusr/dashboard/index.php
+++ ccmsusr/dashboard/index.php
@@ -228,6 +228,19 @@ const gridData = <?php
 
 
 
+const grid = new Grid({
+  columns: ['Name', 'Email', 'Phone Number'],
+  data: [
+    ['John', 'john@example.com', '(353) 01 222 3333'],
+    ['Mark', 'mark@gmail.com',   '(01) 22 888 4444']
+  ]
+});
+
+
+
+
+
+
 
 
 									});
