c86ccefabe78bee11394abe85fa5374cefee64fe
modusinternet <vince@modusinternet.com>
2022-01-22 11:24:15 -0800

    asdf

--- ccmsusr/dashboard/index.php
+++ ccmsusr/dashboard/index.php
@@ -378,7 +378,7 @@ if($_SERVER["SCRIPT_NAME"] != "/ccmsusr/index.php") {
 					+ '</div><div class="tableCell">' + data[i].ip
 					+ '</div><div class="tableCell">' + data[i].url
 					+ '</div><div class="tableCell">' + data[i].log
-					+ '</div><div class="tableCell"><button class="ccms_security_logs_button ccms_security_logs_delete_button" data-id="' + data[i].id + '" title="Delete"></button></div>';
+					+ '</div><div class="tableCell" style="text-align:center"><button class="ccms_security_logs_button ccms_security_logs_delete_button" data-id="' + data[i].id + '" title="Delete"></button></div>';
 
 					divTable.appendChild(divTableRow);
 				}
