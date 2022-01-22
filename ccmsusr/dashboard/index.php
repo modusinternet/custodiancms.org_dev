740e84127f75c52465c4422895f27f150514cbb1
modusinternet <vince@modusinternet.com>
2022-01-22 13:14:25 -0800

    asdf

--- ccmsusr/dashboard/index.php
+++ ccmsusr/dashboard/index.php
@@ -397,10 +397,11 @@ if($_SERVER["SCRIPT_NAME"] != "/ccmsusr/index.php") {
 
 				//(function (){
 					document.querySelectorAll('ccms_security_logs_delete_button').forEach((inp) => {
-						inp.onclick = function () {
+						console.log("delete button pressed for security log id... ");
+						//inp.onclick = function () {
 							//document.getElementById('priceLabel').innerText = "$" + this.dataset.price;
 							console.log("delete button pressed for security log id... ");
-						};
+						//};
 					});
 				//})();
 			}
