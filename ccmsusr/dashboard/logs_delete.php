5a769f8bcc22d70729a0a8709a9b641b1e800273
modusinternet <vince@modusinternet.com>
2022-01-23 12:33:21 -0800

    adsf

--- ccmsusr/dashboard/logs_delete.php
+++ ccmsusr/dashboard/logs_delete.php
@@ -25,7 +25,12 @@ if(!isset($errorMsg['id'])) {
 	// no problems
 	$qry = $CFG["DBH"]->prepare("DELETE FROM `ccms_log` WHERE `id` = :id LIMIT 1;");
 	$qry->execute(array(':id' => $CLEAN["id"]));
-	echo "success test";
+	$count = $del->rowCount();
+	if($count > 0) {
+		echo "0"; // success
+	} else {
+		echo "1"; // already deleted
+	}
 	exit;
 }
 echo $errorMsg['id'];
