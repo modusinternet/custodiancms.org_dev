949233ae118eda1fc22e5a870df25c33bc5af104
modusinternet <vince@modusinternet.com>
2022-03-03 11:54:52 -0800

    asdf

--- ccmsusr/dashboard/index.php
+++ ccmsusr/dashboard/index.php
@@ -42,7 +42,7 @@ if($_SERVER["SCRIPT_NAME"] != "/ccmsusr/index.php") {
 			top:5px
 		}
 
-		#ccms_compress_button:hover{background:unset}
+		/*#ccms_compress_button:hover{background:unset}*/
 
 		#ccms_news_items{padding-left:30px}
 
