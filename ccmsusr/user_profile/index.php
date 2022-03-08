8e021df76be921ba25b6056e72a30ef440fc4f5e
modusinternet <vince@modusinternet.com>
2022-03-08 13:30:08 -0800

    asdf

--- ccmsusr/user_profile/index.php
+++ ccmsusr/user_profile/index.php
@@ -32,10 +32,13 @@ $ccms_user = $qry->fetch(PDO::FETCH_ASSOC);
 
 		.inner_grid_other{grid-area:inner_grid_other}
 
+		.inner_grid_password{grid-area:inner_grid_password}
+
 		.inner_grid_general,
 		.inner_grid_address,
 		.inner_grid_contact,
-		.inner_grid_other{
+		.inner_grid_other,
+		.inner_grid_password{
 			display:grid;
 			grid-gap:8px
 		}
@@ -43,7 +46,8 @@ $ccms_user = $qry->fetch(PDO::FETCH_ASSOC);
 		.inner_grid_general>h3,
 		.inner_grid_address>h3,
 		.inner_grid_contact>h3,
-		.inner_grid_other>h3{
+		.inner_grid_other>h3,
+		.inner_grid_password>h3{
 			margin:10px 0 0;
 			text-align:center
 		}
@@ -51,17 +55,20 @@ $ccms_user = $qry->fetch(PDO::FETCH_ASSOC);
 		.inner_grid_general>input,
 		.inner_grid_address>input,
 		.inner_grid_contact>input,
-		.inner_grid_other>input{height:fit-content}
+		.inner_grid_other>input,
+		.inner_grid_password>input{height:fit-content}
 
 		.inner_grid_general>label,
 		.inner_grid_address>label,
 		.inner_grid_contact>label,
-		.inner_grid_other>label{white-space:nowrap}
+		.inner_grid_other>label,
+		.inner_grid_password>label{white-space:nowrap}
 
 		.inner_grid_general>label.error,
 		.inner_grid_address>label.error,
 		.inner_grid_contact>label.error,
-		.inner_grid_other>label.error{
+		.inner_grid_other>label.error,
+		.inner_grid_password>label.error{
 			text-align:center;
 			white-space:unset;
 			grid-column:unset
@@ -131,12 +138,14 @@ $ccms_user = $qry->fetch(PDO::FETCH_ASSOC);
 			.inner_grid_general,
 			.inner_grid_address,
 			.inner_grid_contact,
-			.inner_grid_other{grid-template-columns:100%}
+			.inner_grid_other,
+			.inner_grid_password{grid-template-columns:100%}
 
 			.inner_grid_general>label.error,
 			.inner_grid_address>label.error,
 			.inner_grid_contact>label.error,
-			.inner_grid_other>label.error{
+			.inner_grid_other>label.error,
+			.inner_grid_password>label.error{
 				text-align:center;
 				white-space:unset;
 				grid-column:1 / 2;
@@ -149,24 +158,29 @@ $ccms_user = $qry->fetch(PDO::FETCH_ASSOC);
 			.inner_grid_general,
 			.inner_grid_address,
 			.inner_grid_contact,
-			.inner_grid_other{grid-template-columns:minmax(100px, 200px) 1fr}
+			.inner_grid_other,
+			.inner_grid_password{grid-template-columns:minmax(100px, 200px) 1fr}
 
-			.inner_grid_other>button{grid-column:1 / span 2}
+			.inner_grid_other>button,
+			.inner_grid_password>button{grid-column:1 / span 2}
 
 			.inner_grid_general>h3,
 			.inner_grid_address>h3,
 			.inner_grid_contact>h3,
-			.inner_grid_other>h3{grid-column:1 / span 2}
+			.inner_grid_other>h3,
+			.inner_grid_password>h3{grid-column:1 / span 2}
 
 			.inner_grid_general>input,
 			.inner_grid_address>input,
 			.inner_grid_contact>input,
-			.inner_grid_other>input{grid-column:2 / 3}
+			.inner_grid_other>input,
+			.inner_grid_password>input{grid-column:2 / 3}
 
 			.inner_grid_general>label,
 			.inner_grid_address>label,
 			.inner_grid_contact>label,
-			.inner_grid_other>label{
+			.inner_grid_other>label,
+			.inner_grid_password>label{
 				grid-column:1 / 2;
 				text-align:right
 			}
@@ -174,7 +188,8 @@ $ccms_user = $qry->fetch(PDO::FETCH_ASSOC);
 			.inner_grid_general>label.error,
 			.inner_grid_address>label.error,
 			.inner_grid_contact>label.error,
-			.inner_grid_other>label.error{grid-column:1 / span 2}
+			.inner_grid_other>label.error,
+			.inner_grid_password>label.error{grid-column:1 / span 2}
 		}
 
 		/* 950px or wider. */
@@ -306,10 +321,10 @@ $ccms_user = $qry->fetch(PDO::FETCH_ASSOC);
 				<div id="password_tab_form_fail" role="alert" style="display:none;text-align:center"></div>
 
 				<form id="password_tab_form" role="form">
-					<div class="inner_grid_general">
+					<div class="inner_grid_password">
 						<input name="ajax_flag" type="hidden" value="1">
 						<h3>Manage Your Password Here</h3>
-						<label for="password">Password <span class="rd">*</span></label>
+						<label for="password">Old Password <span class="rd">*</span></label>
 						<input id="password" name="password" placeholder="Type your current password here." type="password" autocomplete="off" readonly onfocus="this.removeAttribute('readonly');">
 						<label id="password_error" class="error" for="password" style="display:none"></label>
 
@@ -317,7 +332,7 @@ $ccms_user = $qry->fetch(PDO::FETCH_ASSOC);
 						<input id="password1" name="password1" placeholder="Type your new password here." type="password" autocomplete="off" readonly onfocus="this.removeAttribute('readonly');">
 						<label id="password1_error" class="error" for="password1" style="display:none"></label>
 
-						<label for="password2">Repeat New Password <span class="rd">*</span></label>
+						<label for="password2">New Password <span class="rd">*</span></label>
 						<input id="password2" name="password2" placeholder="Type your new password here again." type="password" autocomplete="off" readonly onfocus="this.removeAttribute('readonly');">
 						<label id="password2_error" class="error" for="password2" style="display:none"></label>
 
