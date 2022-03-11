04560bad7f551c3148f15f6a6c58ece80878d8f0
modusinternet <vince@modusinternet.com>
2022-03-11 15:54:59 -0800

    asdf

--- ccmsusr/user_profile/index.php
+++ ccmsusr/user_profile/index.php
@@ -355,7 +355,7 @@ $ccms_user = $qry->fetch(PDO::FETCH_ASSOC);
 							<p>
 								Scan or copy QR code into authentication tool (ie: Google Authenticator) before pressing the Update button.  You will be forced to relogin.
 							</p>
-							<p id="2fa_secret_text"></p>
+							<h1 id="2fa_secret_text"></h1>
 							<svg id="ga_qr_svg" style="display:none;width:75px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50">
 								<path fill="#d7680f" d="M25,5A20.14,20.14,0,0,1,45,22.88a2.51,2.51,0,0,0,2.49,2.26h0A2.52,2.52,0,0,0,50,22.33a25.14,25.14,0,0,0-50,0,2.52,2.52,0,0,0,2.5,2.81h0A2.51,2.51,0,0,0,5,22.88,20.14,20.14,0,0,1,25,5Z">
 									<animateTransform attributeName="transform" type="rotate" from="0 25 25" to="360 25 25" dur="2s" repeatCount="indefinite"/>
