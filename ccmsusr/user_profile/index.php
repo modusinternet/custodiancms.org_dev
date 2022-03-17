9f2ef5217ce4c7ca9af9ec8a32dba461d99ba8ec
modusinternet <vince@modusinternet.com>
2022-03-17 12:11:21 -0700

    asdf

--- ccmsusr/user_profile/index.php
+++ ccmsusr/user_profile/index.php
@@ -657,7 +657,7 @@ fetch('/{CCMS_LIB:_default.php;FUNC:ccms_lng}/user/user_profile/priv_json.php').
 })
 */
 
-const data-old = {
+const data_old = {
   Parent: {
     img: "father.png",
     name: "Jan Doe",
