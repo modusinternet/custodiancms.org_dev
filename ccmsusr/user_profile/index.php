01996c812ae2af67c1542837dc44cbb94733c663
modusinternet <vince@modusinternet.com>
2022-03-17 18:04:15 -0700

    asdf

--- ccmsusr/user_profile/index.php
+++ ccmsusr/user_profile/index.php
@@ -657,70 +657,67 @@ fetch('/{CCMS_LIB:_default.php;FUNC:ccms_lng}/user/user_profile/priv_json.php').
 })
 */
 
-const data_old = {
-  Parent: {
-    img: "father.png",
-    name: "Jan Doe",
-    age: "50",
-    children: [
-      {
-        child: {
-          img: "child_1.png",
-          name: "child 1",
-          age: "25",
-        },
-      },
-      {
-        child: {
-          img: "child_2.png",
-          name: "child 2",
-          age: "22",
-          children: [
-            {
-              grandChild: {
-                img: "child_3.png",
-                name: "grand child 1",
-                age: "12",
-              },
-            },
-          ],
-        },
-      },
-      {
-        child: {
-          img: "child_4.png",
-          name: "child 3",
-          age: "16",
-          children: [
-            {
-              grandChild: {
-                img: "child_5.png",
-                name: "grand child 1",
-                age: "18",
-                children: [
-                  {
-                    grandgrandChild: {
-                      img: "child_6.png",
-                      name: "great grand child 1",
-                      age: "13",
-                    },
-                  },
-                  {
-                    grandgrandChild: {
-                      img: "child_7.png",
-                      name: "great grand child 2",
-                      age: "10",
-                    },
-                  },
-                ],
-              },
-            },
-          ],
-        },
-      },
-    ],
-  },
+const data_old = '{"Parent":{"img":"father.png","name":"Jan Doe","age":"50","children":[{"child":{"img":"child_1.png","name":"child 1","age":"25"}},{"child":{"img":"child_2.png","name":"child 2","age":"22","children":[{"grandChild":{"img":"child_3.png","name":"grand child 1","age":"12"}}]}},{"child":{"img":"child_4.png","name":"child 3","age":"16","children":[{"grandChild":{"img":"child_5.png","name":"grand child 1","age":"18","children":[{"grandgrandChild":{"img":"child_6.png","name":"great grand child 1","age":"13"}},{"grandgrandChild":{"img":"child_7.png","name":"great grand child 2","age":"10"}}]}}]}}]}}';
+
+// array to hold HTML tags
+let markupArray = ["<ul>"];
+
+// evaluate expressions
+const createList = (items) => {
+  switch ($.type(items)) {
+    case "object":
+      getItems(items);
+      break;
+  }
 };
+
+// get items in the object
+const getItems = (items) => {
+  for (const item in items) {
+    markupArray.push(`<li> <div class="test"><span>${item}</span>`);
+    // fetch the parent object
+    let details = items[item];
+    getDetails(details);
+    // push the closing tag for parent
+    markupArray.push("</li>");
+  }
+};
+
+// get details
+const getDetails = (details) => {
+  // iterate over the detail items of object
+  for (const detail in details) {
+    // fetch the value of each item
+    if (detail == "img") {
+      markupArray.push(
+        `<img src="./img/${details[detail]}" alt="${details[detail]}">`
+      );
+    } else if (detail == "children") {
+      markupArray.push("</div><ul>");
+      details[detail].forEach((element) => {
+        getItems(element);
+      });
+
+      markupArray.push("</ul>");
+    } else {
+      markupArray.push(`<span> ${details[detail]} </span>`);
+    }
+  }
+};
+
+createList(data_old);
+markupArray.push("</ul>");
+$("#tab03Content").html(markupArray.join(""));
+
+
+
+
+
+
+
+
+
+
 
 //const data = '< ? = $ccms_user["priv"];?>';
 
@@ -775,6 +772,17 @@ const getDetails = (details) => {
 createList(data);
 markupArray.push("</ul>");
 $("#tab03Content").html(markupArray.join(""));
+
+
+
+
+
+
+
+
+
+
+
 
 
 
