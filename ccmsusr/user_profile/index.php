b1cb117a1f317abe6f8c3f675a715c2461dc217c
modusinternet <vince@modusinternet.com>
2022-03-18 13:38:24 -0700

    asdf

--- ccmsusr/user_profile/index.php
+++ ccmsusr/user_profile/index.php
@@ -712,7 +712,7 @@ const getDetails = (details) => {
   // iterate over the detail items of object
   for (const detail in details) {
     // fetch the value of each item
-    if(details == "admin") {
+    if(detail == "sub") {
 			//markupArray.push("<ul>${details[detail]}");
 			markupArray.push("<ul>");
 			details[detail].forEach((element) => {
