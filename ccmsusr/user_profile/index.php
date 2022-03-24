f02229f652062cffc0857c909cdc75f6116a0093
modusinternet <vince@modusinternet.com>
2022-03-23 23:36:25 -0700

    asdf

--- ccmsusr/user_profile/index.php
+++ ccmsusr/user_profile/index.php
@@ -243,7 +243,7 @@ $ccms_user = $qry->fetch(PDO::FETCH_ASSOC);
 			transition:all 1s;
 		}
 
-		.msg.active{top:0}
+		.msg.active{top:40px}
 	</style>
 	<script nonce="{CCMS_LIB:_default.php;FUNC:ccms_csp_nounce}">
 		let navActiveItem = [];
