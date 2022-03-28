143ab18c90f71d38662cb2a373a1b5550a04bdbd
modusinternet <vince@modusinternet.com>
2022-03-28 13:58:24 -0700

    asdf

--- ccmslib/_default.php
+++ ccmslib/_default.php
@@ -577,8 +577,8 @@ function ccms_user_admin_slider() {
 								ccms_msg_div.classList.remove("active", "error");
 							}
 						}
-						//return false;
-						return;
+						return false;
+						//return;
 						//*/
 
 
