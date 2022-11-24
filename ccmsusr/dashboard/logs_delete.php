99fbe2212d75d8cbfcf5d8f4295987c779c2d81c
modusinternet <vince@modusinternet.com>
2022-11-23 22:04:49 -0800

    asdf

--- ccmsusr/dashboard/logs_delete.php
+++ ccmsusr/dashboard/logs_delete.php
@@ -14,6 +14,9 @@ if($_SERVER["SCRIPT_NAME"] != "/ccmsusr/index.php") {
 $msg = array();
 $privArray = json_decode($_SESSION["PRIV"], true);
 
+echo "_SESSION[SUPER]=[" . $_SESSION["SUPER"] . "]\n";
+echo "privArray[dashboard]=[" . $privArray["dashboard"] . "]\n";
+
 if(ccms_badIPCheck($_SERVER["REMOTE_ADDR"])) {
 	$msg["error"] = "There is a problem with your login, your IP Address is currently being blocked.  Please contact the website administrators directly if you feel this message is in error.";
 
