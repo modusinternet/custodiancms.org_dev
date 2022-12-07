2f2b122982a3bb92f4955836b30f0bdf4222f374
modusinternet <vince@modusinternet.com>
2022-12-06 18:22:48 -0800

    asdf

--- ccmspre/index.php
+++ ccmspre/index.php
@@ -367,11 +367,6 @@ function CCMS_Set_SESSION() {
 		$qry->execute(array(':id' => $_SESSION["USER_ID"]));
 		$row = $qry->fetch(PDO::FETCH_ASSOC);
 
-
-//print_r($row);
-
-
-
 		if($row) {
 			// User 'status' is still valid
 
@@ -382,17 +377,13 @@ function CCMS_Set_SESSION() {
 					// The user is logged in successfully but they have 2FA enabled and haven't verified it yet.
 
 					$CLEAN["ccms_tpl"] = "/authenticator.php";
-				}
-
-				 else {
+				} else {
 					// The user is valid and nothing is outstanding so just update the most current privilages.
 
-					//$_SESSION["2FA_VALID"] = true;
 					$_SESSION["ALIAS"] = $row["alias"];
 					$_SESSION["PRIV"] = $row["priv"];
 					$_SESSION["SUPER"] = $row["super"];
 				}
-
 			} else {
 				// The user is valid and nothing is outstanding so just update the most current privilages.
 
