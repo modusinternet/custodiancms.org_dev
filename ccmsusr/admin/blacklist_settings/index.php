c91dfae9c9a764ae38ad06e8b2f0f44a3879e764
modusinternet <vince@modusinternet.com>
2022-12-14 15:57:11 -0800

    asdf

--- ccmsusr/admin/blacklist_settings/index.php
+++ ccmsusr/admin/blacklist_settings/index.php
@@ -2,7 +2,7 @@
 header("Content-Type: text/html; charset=UTF-8");
 header("Expires: on, 01 Jan 1970 00:00:00 GMT");
 header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
-header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
 
@@ -10,102 +10,279 @@ if($_SERVER["SCRIPT_NAME"] != "/ccmsusr/index.php") {
 	echo "This script can NOT be called directly.";
 	die();
 }
+
+$qry = $CFG["DBH"]->prepare("SELECT * FROM `ccms_user` WHERE `id` = :id LIMIT 1;");
+$qry->execute(array(':id' => $_SESSION["USER_ID"]));
+$ccms_user = $qry->fetch(PDO::FETCH_ASSOC);
+
 ?><!DOCTYPE html>
-<html id="no-fouc" lang="en" style="opacity: 0;">
+<html lang="{CCMS_LIB:_default.php;FUNC:ccms_lng}">
 	<head>
-		<meta charset="utf-8">
-		<title>Blacklist Settings</title>
-		<meta name="description" content="" />
-		{CCMS_TPL:header-head.html}
-		<script>
-			var navActiveArray = ["admin","admin_nav","admin_blacklist_settings"];
-		</script>
+		<title><?= $_SERVER["SERVER_NAME"];?> | User | Blacklist Settings</title>
+		{CCMS_TPL:head-meta.html}
 	</head>
+	<style>
+		{CCMS_TPL:/_css/head-css.html}
+
+		.inner_grid_general{grid-area:inner_grid_general}
+
+		.inner_grid_address{grid-area:inner_grid_address}
+
+		.inner_grid_contact{grid-area:inner_grid_contact}
+
+		.inner_grid_other{grid-area:inner_grid_other}
+
+		.inner_grid_login{grid-area:inner_grid_login}
+
+		.inner_grid_general,
+		.inner_grid_address,
+		.inner_grid_contact,
+		.inner_grid_other,
+		.inner_grid_login{
+			display:grid;
+			grid-gap:8px
+		}
+
+		.inner_grid_general>h3,
+		.inner_grid_address>h3,
+		.inner_grid_contact>h3,
+		.inner_grid_other>h3,
+		.inner_grid_login>h3{
+			margin:10px 0 0;
+			text-align:center
+		}
+
+		.inner_grid_general>input,
+		.inner_grid_address>input,
+		.inner_grid_contact>input,
+		.inner_grid_other>input,
+		.inner_grid_login>input{height:fit-content}
+
+		.inner_grid_general>label,
+		.inner_grid_address>label,
+		.inner_grid_contact>label,
+		.inner_grid_other>label,
+		.inner_grid_login>label{white-space:nowrap}
+
+		.inner_grid_general>label.error,
+		.inner_grid_address>label.error,
+		.inner_grid_contact>label.error,
+		.inner_grid_other>label.error,
+		.inner_grid_login>label.error{
+			text-align:center;
+			white-space:unset;
+			grid-column:unset
+		}
+
+		.outer_grid{
+			display:grid;
+			grid-gap:8px;
+			grid-template-areas:
+				"inner_grid_general"
+				"inner_grid_address"
+				"inner_grid_contact"
+				"inner_grid_other"
+		}
+
+		.tabs{
+			border-bottom:1px solid var(--cl3);
+			overflow:hidden
+		}
+
+		.tabs button{
+			background-color:var(--cl4);
+			border-radius:4px 4px 0 0;
+			color:var(--cl8);
+			cursor:pointer;
+			float:left;
+			font-family:inherit;
+			font-size:inherit;
+			font-weight:inherit;
+			margin-right:2px;
+			outline:none;
+			padding:14px 16px;
+			transition:0.3s;
+			width: unset
+		}
+
+		.tabs button:hover, .tabs button:hover svg path{
+			background-color:var(--cl3);
+			color:var(--cl0)
+		}
+
+		.tabs button.active, .tabs button.active svg path{
+			background-color:var(--cl3);
+			color:var(--cl0)
+		}
+
+		.tabs button:hover svg path{
+			background-color:var(--cl4);
+			fill:var(--cl0)
+		}
+
+		.tabs button.active svg path{
+			background-color:var(--cl4);
+			fill:var(--cl0)
+		}
+
+		.tabs button svg path{fill:var(--cl5)}
+
+		.tabContent{
+			display:none;
+			padding:20px 0px
+		}
+
+		#tab03Content>div>ul{margin-left:20px}
+
+		#tab03Content>div>ul li{padding-left:20px}
+
+
+		/* 435px or wider. */
+		@media only screen and (min-width:435px){
+			.inner_grid_general,
+			.inner_grid_address,
+			.inner_grid_contact,
+			.inner_grid_other,
+			.inner_grid_login{grid-template-columns:100%}
+
+			.inner_grid_general>label.error,
+			.inner_grid_address>label.error,
+			.inner_grid_contact>label.error,
+			.inner_grid_other>label.error,
+			.inner_grid_login>label.error{
+				text-align:center;
+				white-space:unset
+			}
+		}
+
+
+		/* 600px or wider. */
+		@media only screen and (min-width:600px){
+			.inner_grid_general,
+			.inner_grid_address,
+			.inner_grid_contact,
+			.inner_grid_other,
+			.inner_grid_login{grid-template-columns:minmax(100px, 200px) 1fr}
+
+			.inner_grid_other>button,
+			.inner_grid_login>button{grid-column:1 / span 2}
+
+			.inner_grid_login>div{grid-column:2 / 3}
+
+			.inner_grid_general>h3,
+			.inner_grid_address>h3,
+			.inner_grid_contact>h3,
+			.inner_grid_other>h3,
+			.inner_grid_login>h3{grid-column:1 / span 2}
+
+			.inner_grid_general>input,
+			.inner_grid_address>input,
+			.inner_grid_contact>input,
+			.inner_grid_other>input,
+			.inner_grid_login>input{grid-column:2 / 3}
+
+			.inner_grid_general>label,
+			.inner_grid_address>label,
+			.inner_grid_contact>label,
+			.inner_grid_other>label,
+			.inner_grid_login>label{
+				grid-column:1 / 2;
+				text-align:right
+			}
+
+			.inner_grid_general>label.error,
+			.inner_grid_address>label.error,
+			.inner_grid_contact>label.error,
+			.inner_grid_other>label.error,
+			.inner_grid_login>label.error{grid-column:1 / span 2}
+		}
+
+
+		/* 950px or wider. */
+		@media only screen and (min-width:950px){
+			.outer_grid{
+				display:grid;
+				grid-gap:8px;
+				grid-template-areas:
+					"inner_grid_general inner_grid_address"
+					"inner_grid_contact inner_grid_other"
+			}
+		}
+
+
+		/* 1400px or wider. */
+		@media only screen and (min-width:1400px){
+			.outer_grid{
+				display:grid;
+				grid-gap:8px;
+				grid-template-areas:
+					"inner_grid_general inner_grid_address inner_grid_contact"
+					"inner_grid_other inner_grid_other inner_grid_other"
+			}
+		}
+	</style>
+	<script nonce="{CCMS_LIB:_default.php;FUNC:ccms_csp_nounce}">
+		let navActiveItem = [];
+		let navActiveSub = [];
+		//let navActiveW3schoolsItem = ["nav-user_admin_blacklist_settings"];
+	</script>
 	<body>
-		<div id="wrapper">
-			{CCMS_TPL:header-body.php}
+		<main style="padding:20px 20px 20px 0">
+			<h1 style="border-bottom:1px dashed var(--cl3)">Admin | Blacklist Settings</h1>
+			<p>This section is still under development, but if you come across any unresolved issues please let us know at: <a class="ccms_a" href="mailto:info@custodiancms.org?subject=unresolved+issue+report">info@custodiancms.org</a></p>
 
 
-			<div id="page-wrapper">
-				<div class="row">
-					<div class="col-md-12">
-						<h1 class="page-header">Blacklist Settings</h1>
-						<div class="panel panel-danger">
-							<div class="panel-heading">
-								Notice
-							</div>
-							<div class="panel-body">
-								<p>This section of the Custodian CMS admin is currently under development.</p>
-							</div>
-						</div>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nec ligula id nisl fringilla finibus. Vestibulum rhoncus, felis at fringilla ullamcorper, ante mi tincidunt nunc, ac ultrices odio odio vitae lorem. Morbi quis elit id urna efficitur aliquam ut et sapien. Fusce porttitor vel ligula faucibus tempor. Pellentesque tincidunt imperdiet enim, id lobortis ipsum tempus id. In facilisis elementum dictum. Donec suscipit ornare tortor, sed volutpat mauris volutpat at. Pellentesque porttitor ut augue at ultrices. Proin egestas semper lorem quis suscipit. Vivamus eget magna tincidunt, semper sem eu, molestie quam. Praesent nisl velit, ultricies ac malesuada id, dapibus in dui. Mauris luctus velit non mi condimentum rhoncus. Nullam sit amet aliquet turpis, id malesuada nulla. Ut sit amet nisl nec ante commodo eleifend.
 
+			{CCMS_TPL:/footer.html}
+		</main>
 
-					</div>
-				</div>
-			</div>
-		</div>
+		{CCMS_TPL:/body-head.php}
+		<script nonce="{CCMS_LIB:_default.php;FUNC:ccms_csp_nounce}">
+			{CCMS_TPL:/_js/footer-1.php}
 
-		<script>
-			function loadFirst(e,t){var a=document.createElement("script");a.async = true;a.readyState?a.onreadystatechange=function(){("loaded"==a.readyState||"complete"==a.readyState)&&(a.onreadystatechange=null,t())}:a.onload=function(){t()},a.src=e,document.body.appendChild(a)}
+			var l=document.createElement("link");l.rel="stylesheet";
+			l.href = "/ccmsusr/_css/custodiancms.css";
+			var h=document.getElementsByTagName("head")[0];h.parentNode.insertBefore(l,h);
 
-			var cb = function() {
-				var l = document.createElement('link'); l.rel = 'stylesheet';
-				l.href = "/ccmsusr/_css/bootstrap-3.3.7.min.css";
-				var h = document.getElementsByTagName('head')[0]; h.parentNode.insertBefore(l, h);
-
-				var l = document.createElement('link'); l.rel = 'stylesheet';
-				l.href = "/ccmsusr/_css/metisMenu-2.4.0.min.css";
-				var h = document.getElementsByTagName('head')[0]; h.parentNode.insertBefore(l, h);
-
-				var l = document.createElement('link'); l.rel = 'stylesheet';
-				l.href = "/ccmsusr/_css/custodiancms.css";
-				/*l.href = "/ccmsusr/_css/custodiancms.min.css";*/
-				var h = document.getElementsByTagName('head')[0]; h.parentNode.insertBefore(l, h);
-
-				var l = document.createElement('link'); l.rel = 'stylesheet';
-				l.href = "/ccmsusr/_css/font-awesome-4.7.0.min.css";
-				var h = document.getElementsByTagName('head')[0]; h.parentNode.insertBefore(l, h);
-			};
-			
-			var raf = requestAnimationFrame || mozRequestAnimationFrame || webkitRequestAnimationFrame || msRequestAnimationFrame;
-			if (raf) raf(cb);
-			else window.addEventListener('load', cb);
+			var l=document.createElement("link");l.rel="stylesheet";
+			l.href = "/ccmsusr/_css/metisMenu-3.0.6.min.css";
+			var h=document.getElementsByTagName("head")[0];h.parentNode.insertBefore(l,h);
 
 			function loadJSResources() {
-				loadFirst("/ccmsusr/_js/jquery-2.2.0.min.js", function() { /* JQuery is loaded */
-					loadFirst("/ccmsusr/_js/bootstrap-3.3.7.min.js", function() { /* Bootstrap is loaded */
-						loadFirst("/ccmsusr/_js/metisMenu-2.4.0.min.js", function() { /* MetisMenu JavaScript */
-							/*loadFirst("/ccmsusr/_js/custodiancms.js", function() { /* CustodianCMS JavaScript */
-							loadFirst("/ccmsusr/_js/custodiancms.min.js", function() { /* CustodianCMS JavaScript */
+				/*loadFirst("/ccmsusr/_js/jquery-2.2.0.min.js", function() {*/
+				loadFirst("/ccmsusr/_js/jquery-3.6.0.min.js", function() {
+					loadFirst("/ccmsusr/_js/metisMenu-3.0.7.min.js", function() {
+						loadFirst("/ccmsusr/_js/custodiancms.js", function() {
 
-								navActiveArray.forEach(function(s) {$("#"+s).addClass("active");});
 
-								// Load MetisMenu
-								$('#side-menu').metisMenu();
+							/* user_dropdown START */
+							/* When the user clicks on the svg button add the 'show' class to the dropdown box below it. */
+							$("#user_dropdown_btn").click(function() {
+								$("#user_dropdown_list").addClass("show");
+							});
 
-								// Fade in web page.
-								$("#no-fouc").delay(200).animate({"opacity": "1"}, 500);
 
-								$("#menu-toggle").click(function(e) {
-									e.preventDefault();
-									$("#wrapper").toggleClass("toggled");
-									$("#wrapper.toggled").find("#sidebar-wrapper").find(".collapse").collapse("hide");
-									$("#sidebar-wrapper").toggle();
+							/* Hide dropdown menu on click outside */
+							$(document).on("click", function(e){
+								if(!$(e.target).closest("#user_dropdown_btn").length){
+									$("#user_dropdown_list").removeClass("show");
+								}
+							});
+							/* user_dropdown END */
+
+
+							loadFirst("/ccmsusr/_js/jquery-validate-1.19.3.min.js", function() {
+								loadFirst("/ccmsusr/_js/additional-methods-1.17.0.min.js", function() {
+
+
+
+
 								});
-
-
 							});
 						});
 					});
 				});
 			}
-
-			if (window.addEventListener)
-				window.addEventListener("load", loadJSResources, false);
-			else if (window.attachEvent)
-				window.attachEvent("onload", loadJSResources);
-			else window.onload = loadJSResources;
 		</script>
 	</body>
 </html>
