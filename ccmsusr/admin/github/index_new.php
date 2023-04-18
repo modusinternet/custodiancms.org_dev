c0906be4d863b8acb9094f4bb2b1df16a2fae1f1
modusinternet <vince@modusinternet.com>
2023-04-18 12:15:56 -0700

    asdf

--- ccmsusr/admin/github/index_new.php
+++ ccmsusr/admin/github/index_new.php
@@ -234,6 +234,160 @@ $ccms_user = $qry->fetch(PDO::FETCH_ASSOC);
 
 
 
+
+
+
+
+
+
+
+
+
+			<div id="tab01Content" class="tabContent" style="display:block">
+				<div id="info_tab_form_msg" role="alert" class="ccms_msg"></div>
+				<form id="info_tab_form" role="form">
+					<div class="outer_grid">
+						<div class="inner_grid_general">
+							<input name="ajax_flag" type="hidden" value="1">
+							<h3>General</h3>
+							<label for="firstname">Firstname</label>
+							<input id="firstname" name="firstname" placeholder="Type your firstname here." type="text" value="<?php echo $ccms_user["firstname"]; ?>">
+							<label id="firstname_error" class="error" for="firstname" style="display:none"></label>
+
+							<label for="lastname">Lastname</label>
+							<input id="lastname" name="lastname" placeholder="Type your lastname here." type="text" value="<?php echo $ccms_user["lastname"]; ?>">
+							<label id="lastname_error" class="error" for="lastname" style="display:none"></label>
+
+							<label for="alias">Alias <span class="rd">*</span></label>
+							<input id="alias" name="alias" placeholder="Type your alias here." type="text" value="<?php echo $ccms_user["alias"]; ?>">
+							<label id="alias_error" class="error" for="alias" style="display:none"></label>
+
+							<label for="position">Position</label>
+							<input id="position" name="position" placeholder="Type your work Position or Title here." type="text" value="<?php echo $ccms_user["position"]; ?>">
+							<label id="position_error" class="error" for="position" style="display:none"></label>
+						</div>
+
+						<div class="inner_grid_address">
+							<h3>Address</h3>
+							<label for="address1">Address Line #1</label>
+							<input id="address1" name="address1" placeholder="Type your Address here." type="text" value="<?php echo $ccms_user["address1"]; ?>">
+							<label id="address1_error" class="error" for="address1" style="display:none"></label>
+
+							<label for="address2">Address Line #2</label>
+							<input id="address2" name="address2" placeholder="Type other Address info here." type="text" value="<?php echo $ccms_user["address2"]; ?>">
+							<label id="address2_error" class="error" for="address2" style="display:none"></label>
+
+							<label for="prov_state">Prov/State</label>
+							<input id="prov_state" name="prov_state" placeholder="Type your Province or State here." type="text" value="<?php echo $ccms_user["prov_state"]; ?>">
+							<label id="prov_state_error" class="error" for="prov_state" style="display:none"></label>
+
+							<label for="country">Country</label>
+							<input id="country" name="country" placeholder="Type your Country name here." type="text" value="<?php echo $ccms_user["country"]; ?>">
+							<label id="country_error" class="error" for="country" style="display:none"></label>
+
+							<label for="post_zip">Postal/Zipcode</label>
+							<input id="post_zip" name="post_zip" placeholder="Type your Postal/Zipcode here." type="text" value="<?php echo $ccms_user["post_zip"]; ?>">
+							<label id="post_zip_error" class="error" for="post_zip" style="display:none"></label>
+						</div>
+
+						<div class="inner_grid_contact">
+							<h3>Contact</h3>
+							<label for="email">Email <span class="rd">*</span></label>
+							<input id="email" name="email" data-rule-required="true" data-rule-email="true" placeholder="Type your email address here." type="text" value="<?php echo $ccms_user["email"]; ?>">
+							<label id="email_error" class="error" for="email" style="display:none"></label>
+
+							<label for="phone1">Phone #1</label>
+							<input id="phone1" name="phone1" placeholder="Type your main phone number here." type="text" value="<?php echo $ccms_user["phone1"]; ?>">
+							<label id="phone1_error" class="error" for="phone1" style="display:none"></label>
+
+							<label for="phone2">Phone #2</label>
+							<input id="phone2" name="phone2" placeholder="Type your secondary phone number here." type="text" value="<?php echo $ccms_user["phone2"]; ?>">
+							<label id="phone2_error" class="error" for="phone2" style="display:none"></label>
+
+							<label for="facebook">Facebook</label>
+							<input id="facebook" name="facebook" placeholder="Type your facebook URI here." type="text" value="<?php echo $ccms_user["facebook"]; ?>">
+							<label id="facebook_error" class="error" for="facebook" style="display:none"></label>
+
+							<label for="skype">Skype</label>
+							<input id="skype" name="skype" placeholder="Type your skype account name here." type="text" value="<?php echo $ccms_user["skype"]; ?>">
+							<label id="skype_error" class="error" for="skype" style="display:none"></label>
+						</div>
+
+						<div class="inner_grid_other">
+							<h3>Other</h3>
+							<label for="note" class="control-label">Notes</label>
+							<textarea id="note" name="note" placeholder="Type any notes you want to save within your account here." rows="4"><?php echo $ccms_user["note"]; ?></textarea>
+							<label id="note_error" class="error" for="note" style="display:none"></label>
+
+							<button>Update</button>
+						</div>
+					</div>
+				</form>
+			</div>
+
+			<div id="tab02Content" class="tabContent">
+				<div id="password_tab_form_msg" role="alert" class="ccms_msg"></div>
+				<form id="password_tab_form" role="form">
+					<div class="inner_grid_login">
+						<input name="ajax_flag" type="hidden" value="1">
+						<h3>Password</h3>
+						<label for="ccms_login_password">Old Password <span class="rd">*</span></label>
+						<input id="ccms_login_password" name="ccms_login_password" placeholder="Type your current password here." type="password">
+						<label id="ccms_login_password_error" class="error" for="ccms_login_password" style="display:none"></label>
+
+						<label for="ccms_pass_reset_part_2_pass_1">New Password</label>
+						<input id="ccms_pass_reset_part_2_pass_1" name="ccms_pass_reset_part_2_pass_1" placeholder="Type your new password here." type="password">
+						<label id="ccms_pass_reset_part_2_pass_1_error" class="error" for="ccms_pass_reset_part_2_pass_1" style="display:none"></label>
+
+						<label for="ccms_pass_reset_part_2_pass_2">Retype</label>
+						<input id="ccms_pass_reset_part_2_pass_2" name="ccms_pass_reset_part_2_pass_2" placeholder="Re-type your new password here." type="password">
+						<label id="ccms_pass_reset_part_2_pass_2_error" class="error" for="ccms_pass_reset_part_2_pass_2" style="display:none"></label>
+						<label for="2fa_checkbox" title="2-Factor Authentication">
+							2FA?
+							<a href="https://authy.com/what-is-2fa/" target="_blank" title="What is 2FA">
+								<svg style="width:20px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill="#d7680f" d="M18,10.82a1,1,0,0,0-1,1V19a1,1,0,0,1-1,1H5a1,1,0,0,1-1-1V8A1,1,0,0,1,5,7h7.18a1,1,0,0,0,0-2H5A3,3,0,0,0,2,8V19a3,3,0,0,0,3,3H16a3,3,0,0,0,3-3V11.82A1,1,0,0,0,18,10.82Zm3.92-8.2a1,1,0,0,0-.54-.54A1,1,0,0,0,21,2H15a1,1,0,0,0,0,2h3.59L8.29,14.29a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0L20,5.41V9a1,1,0,0,0,2,0V3A1,1,0,0,0,21.92,2.62Z"/></svg>
+							</a>
+						</label>
+						<input id="2fa_secret" type="hidden" name="2fa_secret">
+						<div>
+							<input type="radio" id="2fa_radio_0" name="2fa_radio" value="0">
+							<label id="2fa_radio_0_label" for="2fa_radio_0">2FA Enabled</label><br>
+							<input type="radio" id="2fa_radio_1" name="2fa_radio" value="1">
+							<label for="2fa_radio_1">2FA Disabled</label><br>
+							<input type="radio" id="2fa_radio_2" name="2fa_radio" value="2">
+							<label for="2fa_radio_2">Generate new 2FA QR code</label>
+						</div>
+						<div id="ga_qr_div" style="display:none">
+							<p>
+								Scan or copy the QR code below into an authentication tool (ie: Google Authenticator) before you press the Update button.  You are going to be forced to relogin.
+							</p>
+							<h3 id="2fa_secret_text" style="margin-top:30px"></h3>
+							<svg id="ga_qr_svg" style="display:none;width:75px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50">
+								<path fill="#d7680f" d="M25,5A20.14,20.14,0,0,1,45,22.88a2.51,2.51,0,0,0,2.49,2.26h0A2.52,2.52,0,0,0,50,22.33a25.14,25.14,0,0,0-50,0,2.52,2.52,0,0,0,2.5,2.81h0A2.51,2.51,0,0,0,5,22.88,20.14,20.14,0,0,1,25,5Z">
+									<animateTransform attributeName="transform" type="rotate" from="0 25 25" to="360 25 25" dur="2s" repeatCount="indefinite"/>
+								</path>
+							</svg>
+							<img id="ga_qr_img" style="display:none" />
+						</div>
+						<button>Update</button>
+					</div>
+				</form>
+			</div>
+
+			<div id="tab03Content" class="tabContent">
+				<p>
+					Access too and the use of functions within the 'User' area are broken into 3 privilege levels.  <span style="color:var(--cl11)">No access</span>, <span style="color:var(--cl4)">read only access</span>, or <span style="color:var(--cl3)">read and write access</span>.
+				</p>
+				<div id="privTree"></div>
+			</div>
+
+
+
+
+			
+
+
+
 			{CCMS_TPL:/footer.html}
 		</main>
 
