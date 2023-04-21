34da53f44e2e97fbd19291d143c0c709f82d0841
modusinternet <vince@modusinternet.com>
2023-04-21 02:20:42 -0700

    asdf

--- ccmsusr/admin/github/index_new.php
+++ ccmsusr/admin/github/index_new.php
@@ -156,10 +156,6 @@ if(!is_callable('shell_exec') && true === stripos(ini_get('disable_functions'), 
 						<p>.git is either NOT installed or you do not have access to git from this account.  Confirm with your administrator before continuing.</p>
 						<pre style="padding: 15px; margin: 15px 0px 20px;"><?=$msg["git"]["error"];?></pre>
 <? else: ?>
-						<p>
-							else<br />
-							<? echo '<pre>'; print_r($msg); echo '</pre>'; ?>
-						</p>
 	<? if(isset($msg["git"]["status"]["error"])): ?>
 						<p>No .git repository setup in this directory or any of it's parent directories yet.  <a class="href-to-setup" href="#setup">Click here</a> to learn more about how to set up and connect this website to your own GitHub repository.</p>
 						<pre style="padding:15px;margin:15px 0px 20px">fatal: not a git repository (or any of the parent directories): .git</pre>
