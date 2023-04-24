aa545e3d577d6bcc38e63d5cd9bb028f2366477b
modusinternet <vince@modusinternet.com>
2023-04-24 09:21:36 -0700

    asdf

--- ccmsusr/admin/github/index_new.php
+++ ccmsusr/admin/github/index_new.php
@@ -147,62 +147,53 @@ if(!is_callable('shell_exec') && true === stripos(ini_get('disable_functions'), 
 
 
 			<div id="tab01Content" class="tabContent" style="display:block">
-
 <? if(isset($msg["shell_exce"]["error"])): ?>
-						<p>Unable to call shell_exce().  Confirm your account has access to this function with your administrator before continuing.</p>
+				<p>Unable to call shell_exce().  Confirm your account has access to this function with your administrator before continuing.</p>
 <? elseif(isset($msg["git"]["error"])): ?>
-						<p>.git is either NOT installed or you do not have access to git from this account.  Confirm with your administrator before continuing.</p>
-						<pre style="padding: 15px; margin: 15px 0px 20px;"><?=$msg["git"]["error"];?></pre>
+				<p>.git is either NOT installed or you do not have access to git from this account.  Confirm with your administrator before continuing.</p>
+				<pre style="padding: 15px; margin: 15px 0px 20px;"><?=$msg["git"]["error"];?></pre>
 <? else: ?>
-						<h2>git status</h2>
-	<? if(isset($msg["git"]["status"]["error"])): ?>
-						<p>No .git repository setup in this directory or any of it's parent directories yet.  <a class="href-to-setup" href="#setup">Click here</a> to learn more about how to set up and connect this website to your own GitHub repository.</p>
-						<pre style="padding:15px;margin:15px 0px 20px">fatal: not a git repository (or any of the parent directories): .git</pre>
-	<? elseif(isset($msg["git"]["status"]["warning"])): ?>
-						<p>There is something wrong with this repository, you might need to access it from the command-line and run add/commit/push manunally to fix it.</p>
-						<pre style="padding: 15px; margin: 15px 0px 20px;"><?=$msg["git"]["status"]["warning"];?></pre>
-						<p>(Easier to read file list, remember all files listed are located relative to the document root of your website.)</p>
-						<pre style="padding: 15px; margin: 15px 0px 20px;"><?=$msg["git"]["status2"]["output"];?></pre>
-						<p>Note: Pushing from your server to a GitHub repository is not recommended for security reasons which is why it is not an automated feature in Custodian CMS.  Use the two commands below if needed.</p>
-						<p class="boxed">
-							git commit -am "from server"<br>
-							git push
-						</p>
-						<p>
-							Note: Or, if all you want to do is overwrite a single file on your server with what's currently on the GitHub repo you can try the following command. (NOTE: You may need to navigate into the dir that contains the file you want to overwrite first.)
-						</p>
-						<p class="boxed">
-							git checkout origin/master -- {filename}<br>
-							git checkout -- .htaccess<br>
-							git checkout origin/main -- ccmstpl/examples/index.html
-						</p>
-	<? else: ?>
-					<p>Success</p>
-					<pre style="padding: 15px; margin: 15px 0px 20px;"><?=$msg["git"]["status"];?></pre>
+				<h2>git status</h2>
+<? if(isset($msg["git"]["status"]["error"])): ?>
+				<p>No .git repository setup in this directory or any of it's parent directories yet.  <a class="href-to-setup" href="#setup">Click here</a> to learn more about how to set up and connect this website to your own GitHub repository.</p>
+				<pre style="padding:15px;margin:15px 0px 20px">fatal: not a git repository (or any of the parent directories): .git</pre>
+<? elseif(isset($msg["git"]["status"]["warning"])): ?>
+				<p>There is something wrong with this repository, you might need to access it from the command-line and run add/commit/push manunally to fix it.</p>
+				<pre style="padding: 15px; margin: 15px 0px 20px;"><?=$msg["git"]["status"]["warning"];?></pre>
+				<p>(Easier to read file list, remember all files listed are located relative to the document root of your website.)</p>
+				<pre style="padding: 15px; margin: 15px 0px 20px;"><?=$msg["git"]["status2"]["output"];?></pre>
+				<p>Note: Pushing from your server to a GitHub repository is not recommended for security reasons which is why it is not an automated feature in Custodian CMS.  Use the two commands below if needed.</p>
+				<p class="boxed">
+					git commit -am "from server"<br>
+					git push
+				</p>
+				<p>Note: Or, if all you want to do is overwrite a single file on your server with what's currently on the GitHub repo you can try the following command. (NOTE: You may need to navigate into the dir that contains the file you want to overwrite first.)</p>
+				<p class="boxed">
+					git checkout origin/master -- {filename}<br>
+					git checkout -- .htaccess<br>
+					git checkout origin/main -- ccmstpl/examples/index.html
+				</p>
+<? else: ?>
+				<p>Success</p>
+				<pre style="padding: 15px; margin: 15px 0px 20px;"><?=$msg["git"]["status"];?></pre>
 	<? endif ?>
 <? endif ?>
-
 			</div>
 
 
 
 
 			<div id="tab02Content" class="tabContent">
-				<div class="modal">
-					<div></div>
-					<div>
-						<h2>git --version</h2>
-						<pre style="padding: 15px; margin: 15px 0px 20px;"><?=$msg["git"]["version"];?></pre>
-						<h2>git config --list</h2>
-						<pre style="padding: 15px; margin: 15px 0px 20px;"><?=$msg["git"]["config"];?></pre>
-						<h2>.gitignore</h2>
+				<h2>git --version</h2>
+				<pre style="padding: 15px; margin: 15px 0px 20px;"><?=$msg["git"]["version"];?></pre>
+				<h2>git config --list</h2>
+				<pre style="padding: 15px; margin: 15px 0px 20px;"><?=$msg["git"]["config"];?></pre>
+				<h2>.gitignore</h2>
 <? if(isset($msg["gitignore"])): ?>
-						<pre style="padding: 15px; margin: 15px 0px 20px;"><?=$msg["gitignore"];?></pre>
+				<pre style="padding: 15px; margin: 15px 0px 20px;"><?=$msg["gitignore"];?></pre>
 <? else: ?>
-						<pre style="padding: 15px; margin: 15px 0px 20px;">.gitignore not found.</pre>
+				<pre style="padding: 15px; margin: 15px 0px 20px;">.gitignore not found.</pre>
 <? endif ?>
-					</div>
-				</div>
 			</div>
 
 
@@ -210,48 +201,43 @@ if(!is_callable('shell_exec') && true === stripos(ini_get('disable_functions'), 
 
 
 			<div id="tab03Content" class="tabContent">
-				<div class="modal">
-					<div></div>
-					<div>
-						<p style="margin: 15px 0px;">Listed below are the basic setup details to connect your website to a GitHub repository.  For more information about how to setup and maintain Git on your server visit <a href="https://git-scm.com/docs" target="_blank">https://git-scm.com/docs</a>.</p>
-						<h2>Repository and Webserver Setup</h2>
-						<ol class="boxed">
-							<li>Create a new repository at GitHub. (<a href="https://github.com" target="_blank">https://github.com</a>)</li>
-							<li>Add your web servers public ssh-key (id_rsa.pub) to your GitHub account under "Settings/SSH and GPG keys". (Follow instructions here to generate a new ssh-key if needed: <a href="https://help.github.com/articles/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent/" target="_blank">https://help.github.com/articles/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent/</a>)</li>
-							<li>Add a webhook on GitHub under "Settings/Webhooks": https://<?=$CFG["DOMAIN"];?>/ccmsusr/github/webhook.php</li>
-							<li>Create a new website folder on your server. (You must have access to shell, ssh and git services.)</li>
-						</ol>
-						<h2>Copy Custondian CMS Templates to Webserver</h2>
-						<p style="margin: 15px 0px;">You can download the latest master version of the Custodian CMS templates from <a href="https://github.com/modusinternet/Custodian-CMS/archive/master.zip" target="_blank">GitHub</a> directly or use the <a href="https://github.com/modusinternet/Custodian-CMS-Download" target="_blank">Custodian CMS Download</a>.  If you prefer SSH, log into your server and type the following on the command-line.</p>
-						<ol class="boxed">
-							<li>git clone --depth=1 https://github.com/modusinternet/Custodian-CMS.git /tmp/Custodian-CMS</li>
-							<li>rm -rf /tmp/Custodian-CMS/.git</li>
-							<li>shopt -s dotglob</li>
-							<li>cp -r /tmp/Custodian-CMS/* /THE_PATH_TO_YOUR_WEBSITES_DOCUMMENT_ROOT</li>
-							<li>rm -rf /tmp/Custodian-CMS</li>
-						</ol>
-						<h2>Initialize git on the Webserver</h2>
-						<p style="margin: 15px 0px;">Once you've finished moving a copy of the Custodian CMS templates into place initialize git at the document root of the website and connect it to your GitHub repository.</p>
-						<ol class="boxed">
-							<li>Test your connection to the GitHub servers via ssh:<br>
-								ssh -T git@github.com<br>
-								If successful, type the following commands:</li>
-							<li>git init</li>
-							<li>git add .</li>
-							<li>git config --global user.email "noreply@<?=$CFG["DOMAIN"];?>"</li>
-							<li>git config --global user.name "YOUR_NAME"</li>
-							<li>git commit -m "first commit"</li>
-							<li>git remote add origin git@github.com:YOUR_ACCOUNT_ON_GITHUB/YOUR_REPO_ON_GITHUB.git</li>
-							<li>git push -u origin master</li>
-						</ol>
-						<h2>Install Local Software</h2>
-						<ol class="boxed">
-							<li>Check GitHub to see if all the files on your web server have been copied over.</li>
-							<li>Install GitHub Desktop (<a href="https://desktop.github.com" target="_blank">https://desktop.github.com</a>) on your PC and File/Clone Repository to somewhere on your computer.</li>
-							<li>Install the Atom editor (<a href="https://atom.io" target="_blank">https://atom.io</a>) and go to "File/Add Project Folder" and select the document root folder containing the local copy of your repositories.  You should now be able to make changes using Atom, commit your changes to GitHub which will automaticaly submit them to your live website using the webhook.</li>
-						</ol>
-					</div>
-				</div>
+				<p style="margin: 15px 0px;">Listed below are the basic setup details to connect your website to a GitHub repository.  For more information about how to setup and maintain Git on your server visit <a href="https://git-scm.com/docs" target="_blank">https://git-scm.com/docs</a>.</p>
+				<h2>Repository and Webserver Setup</h2>
+				<ol class="boxed">
+					<li>Create a new repository at GitHub. (<a href="https://github.com" target="_blank">https://github.com</a>)</li>
+					<li>Add your web servers public ssh-key (id_rsa.pub) to your GitHub account under "Settings/SSH and GPG keys". (Follow instructions here to generate a new ssh-key if needed: <a href="https://help.github.com/articles/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent/" target="_blank">https://help.github.com/articles/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent/</a>)</li>
+					<li>Add a webhook on GitHub under "Settings/Webhooks": https://<?=$CFG["DOMAIN"];?>/ccmsusr/github/webhook.php</li>
+					<li>Create a new website folder on your server. (You must have access to shell, ssh and git services.)</li>
+				</ol>
+				<h2>Copy Custondian CMS Templates to Webserver</h2>
+				<p style="margin: 15px 0px;">You can download the latest master version of the Custodian CMS templates from <a href="https://github.com/modusinternet/Custodian-CMS/archive/master.zip" target="_blank">GitHub</a> directly or use the <a href="https://github.com/modusinternet/Custodian-CMS-Download" target="_blank">Custodian CMS Download</a>.  If you prefer SSH, log into your server and type the following on the command-line.</p>
+				<ol class="boxed">
+					<li>git clone --depth=1 https://github.com/modusinternet/Custodian-CMS.git /tmp/Custodian-CMS</li>
+					<li>rm -rf /tmp/Custodian-CMS/.git</li>
+					<li>shopt -s dotglob</li>
+					<li>cp -r /tmp/Custodian-CMS/* /THE_PATH_TO_YOUR_WEBSITES_DOCUMMENT_ROOT</li>
+					<li>rm -rf /tmp/Custodian-CMS</li>
+				</ol>
+				<h2>Initialize git on the Webserver</h2>
+				<p style="margin: 15px 0px;">Once you've finished moving a copy of the Custodian CMS templates into place initialize git at the document root of the website and connect it to your GitHub repository.</p>
+				<ol class="boxed">
+					<li>Test your connection to the GitHub servers via ssh:<br>
+						ssh -T git@github.com<br>
+						If successful, type the following commands:</li>
+					<li>git init</li>
+					<li>git add .</li>
+					<li>git config --global user.email "noreply@<?=$CFG["DOMAIN"];?>"</li>
+					<li>git config --global user.name "YOUR_NAME"</li>
+					<li>git commit -m "first commit"</li>
+					<li>git remote add origin git@github.com:YOUR_ACCOUNT_ON_GITHUB/YOUR_REPO_ON_GITHUB.git</li>
+					<li>git push -u origin master</li>
+				</ol>
+				<h2>Install Local Software</h2>
+				<ol class="boxed">
+					<li>Check GitHub to see if all the files on your web server have been copied over.</li>
+					<li>Install GitHub Desktop (<a href="https://desktop.github.com" target="_blank">https://desktop.github.com</a>) on your PC and File/Clone Repository to somewhere on your computer.</li>
+					<li>Install the Atom editor (<a href="https://atom.io" target="_blank">https://atom.io</a>) and go to "File/Add Project Folder" and select the document root folder containing the local copy of your repositories.  You should now be able to make changes using Atom, commit your changes to GitHub which will automaticaly submit them to your live website using the webhook.</li>
+				</ol>
 			</div>
 
 
