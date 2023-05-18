5f2f6d7a843c6313e726b39fc00d35edc4a991b4
modusinternet <vince@modusinternet.com>
2023-05-18 01:26:41 -0700

    asdf

--- ccmslib/_default.php
+++ ccmslib/_default.php
@@ -1,4 +1,39 @@
 <?php
+/*
+$aws_flag = if not null append AWS link.
+$lng_flag = if not null append language code to link.
+$path = a variable found in the config file that represents a partial pathway to the style sheet. (Not including details about AWS, language code, or language direction.)
+$dir_flag = if not null append language direction to link
+$ver_numb, this is very helpful when trying to update files like css and js that don't get called by serviceWorker after they are stored. (empty = do not append '?v=some_number' to the URL.)
+Example: {CCMS_LIB:_default.php;FUNC:ccms_build_js_link("","","JS-01","","1")}
+*/
+function ccms_build_js_link($aws_flag, $lng_flag, $path, $dir_flag, $ver_numb){
+	global $CFG;
+	// If $path is not found in the config.php file then do nothing.
+	if(!isset($CFG["RES"][$path])) return;
+	$url = "";
+	if($aws_flag){
+		if($CFG["RES"]["AWS"]){
+			$url .= $CFG["RES"]["AWS"];
+		}
+	}
+	// We do this for safety to help just incase the script calling this function requests the AWS code and the language code by accident.  We never ask for language code ones things are located on AWS.
+	if($lng_flag){
+		if(!$aws_flag){
+			$url .= "/" . ccms_lng_ret();
+		}
+	}
+	$url .= $CFG["RES"][$path];
+	if($dir_flag){
+		$url .= "-" . ccms_lng_dir_ret();
+	}
+	$url .= '.js';
+	if($ver_numb){
+		$url .= "?v=" . $ver_numb;
+	}
+	echo $url;
+}
+
 function ccms_cfgDomain() {
 	global $CFG;
 	echo $CFG["DOMAIN"];
