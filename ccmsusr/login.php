7e8eecd73f332dd671a05ba74f313131e1930591
modusinternet <vince@modusinternet.com>
2024-04-04 16:22:10 -0700

    asdf

--- ccmsusr/login.php
+++ ccmsusr/login.php
@@ -78,7 +78,7 @@ if(isset($_SESSION['EXPIRED']) == "1") {
 			$resp = json_decode($resp, true);
 
 			if($resp["success"] == false || $resp["action"] !== $CLEAN["g-recaptcha-action"] || $resp["score"] <= 0.4) {
-				$ccms_login_message["FAIL"] = 'Google reCAPTCHA failed or expired. Try again.';
+				$ccms_login_message["FAIL"] = 'Google reCAPTCHA failed or expired. Try again. (1)';
 				//$ccms_login_message["FAIL"] = 'Google reCAPTCHA failed or expired. Try again. (success=['.$resp["success"].'], score=['.$resp["score"].'], action=['.$resp["action"].'], error-codes=['.$resp["error-codes"].'])';
 			}
 
@@ -433,7 +433,7 @@ $email_message .= "\r\n\r\n--" . $boundary . "--";
 			$resp = json_decode($resp, true);
 
 			if($resp["success"] == false || $resp["action"] !== $CLEAN["g-recaptcha-action"] || $resp["score"] <= 0.4) {
-				$ccms_pass_reset_message["FAIL"] = 'Google reCAPTCHA failed or expired. Try again.';
+				$ccms_pass_reset_message["FAIL"] = 'Google reCAPTCHA failed or expired. Try again. (2)';
 				//$ccms_pass_reset_message["FAIL"] = 'Google reCAPTCHA failed or expired. Try again. (success=['.$resp["success"].'], score=['.$resp["score"].'], action=['.$resp["action"].'], error-codes=['.$resp["error-codes"].'])';
 			}
 
